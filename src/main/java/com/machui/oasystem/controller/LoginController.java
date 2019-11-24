655723d6dfd45c2d0f1e4f803d3f777cab45fc80
LeeYaonan <leeyaonan@outlook.com>
2019-11-24 22:42:03 +0800

    cors

--- src/main/java/com/machui/oasystem/controller/LoginController.java
+++ src/main/java/com/machui/oasystem/controller/LoginController.java
@@ -1,16 +1,19 @@
 package com.machui.oasystem.controller;
 
 import com.machui.oasystem.common.model.ApiResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class LoginController {
     private final String TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImNyZWF0ZWQiOjE1NzQ2MDI5MjcwNDEsImV4cCI6MTU3NTIwNzcyN30.fgObAKJZxdEkydT2HPcQAjtJpqT45q1ACUDe6XRMP4h4aMNOJPoiVWMJB0AQUaFqWfrbJgouyKV2DygJd7_4Zw";
     private final String TOKEN_HEAD = "Bearer";
 
-    @RequestMapping("/login")
+    @PostMapping("/admin/auth/login")
     public ApiResult login(){
         HashMap<String, String> data = new HashMap<>();
         data.put("token", TOKEN);
@@ -18,7 +21,7 @@ public class LoginController {
         ApiResult<Object> apiResult = new ApiResult<>();
         apiResult.setCode(200);
         apiResult.setData(data);
-        apiResult.setMessage("success");
+        apiResult.setMessage("操作成功");
         return apiResult;
 
     }
