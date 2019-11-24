cfc1303bb3fa1a12e6fef3879b7bbe4f5d4be16f
LeeYaonan <leeyaonan@outlook.com>
2019-11-24 22:17:00 +0800

    lombok

--- src/main/java/com/machui/oasystem/common/model/ApiResult.java
+++ src/main/java/com/machui/oasystem/common/model/ApiResult.java
@@ -1,33 +1,12 @@
 package com.machui.oasystem.common.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class ApiResult<T> implements Serializable {
     private int code;
     private Object Data;
     private String message;
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public Object getData() {
-        return Data;
-    }
-
-    public void setData(Object data) {
-        Data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
